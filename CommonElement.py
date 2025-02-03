2ac32f5f74083aba23bbc7ef1ea3d33550eda2ab
celery-sickles <elleryahn@gmail.com>
2025-02-03 13:47:09 -0500

    Added CommonElement.py

---
+++ CommonElement.py
@@ -0,0 +1,10 @@
+def CommonElement(array1, array2):
+    array3 = []
+    for i in array1:
+        for j in array2:
+            if i == j:
+                array3.append(i)
+    return array3
+
+
+print(CommonElement([1,9,6,4,5],[6,7,8,9]))
